fecffde2a5da4554bb2f7a80d27587ad2334db12
Florian Hitzler <20150060@HTBLAPerg.onmicrosoft.com>
2019-03-14 20:59:21 +0100

    added angular materials, basic form, get method

--- app-todo-list/src/app/app.module.ts
+++ app-todo-list/src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+import { InputComponent } from "./input/input.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, InputComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule, BrowserAnimationsModule, MatButtonModule, MatCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
